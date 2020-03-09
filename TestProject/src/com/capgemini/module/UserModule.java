3cb61058147e93a421e75b5088f8ea4d467ec136
Evision <Evision@DESKTOP-HLDOKET>
2020-03-09 11:06:17 +0530

    dev 2

--- TestProject/src/com/capgemini/module/UserModule.java
+++ TestProject/src/com/capgemini/module/UserModule.java
@@ -4,4 +4,8 @@ public class UserModule {
 	public void registration() {
 		System.out.println("registration functionality");
 	}
+
+public void registration1() {
+	System.out.println("registration functionality");
+}
 }
